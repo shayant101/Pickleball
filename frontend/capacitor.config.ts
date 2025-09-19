c0f463a04d8813e588c041f60a6a7fa192d0678a
user1 <user1@Mac-Black.local>
2025-09-19 07:56:55 -0700

    feat: update and save changes

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.crimsonhummingbirdcrawl',
+  appName: 'crimson-hummingbird-crawl',
+  webDir: 'dist'
+};
+
+export default config;
